4e12e7b854c18963494a08b0f6c21fc2cc4069f5
RoeiA98 <roei922@gmail.com>
2023-12-10 00:45:26 +0200

    day4

---
+++ day4/part1.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void) {
+
+    FILE* input = fopen("input1.txt", "r");
+    int counter = 0;
+    int a = 0, b = 0, c = 0, d = 0;
+
+    while(fscanf(input,"%d-%d, %d-%d\n", &a, &b, &c, &d) > 0) { // ! fscanf() READS TILL EOF
+
+        if (a <= c && b >= d) {
+            counter++;
+        } else if (a >= c && b <= d) {
+            counter++;
+        }
+
+        // printf("%d %d %d %d\n", a, b, c, d); 
+    }
+
+    printf("answer: %d\n", counter);
+    fclose(input);
+    return 0;
+}
